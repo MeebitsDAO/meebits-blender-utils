b163dba93a8a765c26239c3aeee28494ed9d5b91
Dagfinn Parnas <dagfinn.parnas@gmail.com>
2021-06-15 21:14:59 +0200

    Also convert fbx from docker

--- docker/code/convert_all_meebits.sh
+++ docker/code/convert_all_meebits.sh
@@ -3,5 +3,7 @@
 for meebit_file in /meebits/*solid.vox 
 do
     blender MeebitRig.blend --background --python meebit_export_to_vrm.py -- --meebit $meebit_file
+    blender MeebitRig.blend --background --python meebit_export_to_fbx.py -- --meebit $meebit_file
     mv -f *.vrm output_vrm/
+    mv -f *.fbx output_vrm/
 done

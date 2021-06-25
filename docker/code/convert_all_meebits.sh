08d1c9f4704a76b401a2afa2a5f9042f3fdc9994
Dagfinn Parnas <dagfinn.parnas@gmail.com>
2021-06-25 22:28:11 +0200

    Fix in convert_all_meebits.sh

--- docker/code/convert_all_meebits.sh
+++ docker/code/convert_all_meebits.sh
@@ -3,7 +3,7 @@
 for meebit_file in /meebits/*solid.vox 
 do
     blender MeebitRig.blend --background --python meebit_export_to_vrm.py -- --meebit $meebit_file
-    blender MeebitRig.blend --background --python meebit_export_to_nonvrmpy -- --meebit $meebit_file
+    blender MeebitRig.blend --background --python meebit_export_to_nonvrm.py -- --meebit $meebit_file
     mv -f *.vrm output_vrm/
     mv -f *.obj output_vrm/
     mv -f *.fbx output_vrm/
